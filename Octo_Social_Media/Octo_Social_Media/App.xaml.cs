33df4a9b86c2674613c4e31021ddff836842d2f2
Chloe Hawthorne <cmhawthorne02@gmail.com>
2020-03-20 08:29:34 +0000

    The Beginning

---
+++ Octo_Social_Media/Octo_Social_Media/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Octo_Social_Media.Services;
+using Octo_Social_Media.Views;
+
+namespace Octo_Social_Media
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Octo_Social_Media/Octo_Social_Media/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Octo_Social_Media.Models;
+
+namespace Octo_Social_Media.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
