92957f9c4a67d103ed29b490be0a8c9e84039848
Chloe Hawthorne <cmhawthorne02@gmail.com>
2020-06-04 07:57:19 +0100

    recent changes, doesnt work yet

---
+++ Octo_Social_Media/Octo_Social_Media/ViewModels/InboxViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Octo_Social_Media.Models;
+using Octo_Social_Media.Views;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Octo_Social_Media.ViewModels
+{
+   public  class InboxViewModel : LandingPage
+    {
+        ObservableCollection<InboxModel> inbox;
+        public ObservableCollection<InboxModel> Inbox
+        {
+            get => inbox;
+            set
+            {
+                inbox = value;
+                
+            }
+        }
+        public InboxViewModel()
+        {
+            CreateCells();
+        }
+        public void CreateCells()
+        {
+            ObservableCollection<InboxModel> list = new ObservableCollection<InboxModel>();
+
+            list.Add(new InboxModel() { Username = "Username01", Message = "Here's a message that is longer than 50 characters just wanted to see how this would work" });
+            list.Add(new InboxModel() { Username = "Username02", Message = "Here's a message" });
+            list.Add(new InboxModel() { Username = "AlexHam01", Message = "There's a million things I haven't done but just you wait" });
+            list.Add(new InboxModel() { Username = "JohnC3na", Message = "u can't see me :p" });
+            Inbox = list ;
+        }
+        /*#region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion*/
+    }
+}
---
+++ Octo_Social_Media/Octo_Social_Media/Views/InboxListViewPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Octo_Social_Media.Views
+{
+   //[XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class InboxListViewPage : ContentPage
+    { 
+        public InboxListViewPage()
+        {
+            InitializeComponent();
+
+         }
+
+        
+    }
+}
